4e4de7bbd261ee30f230b47e2b1c3b543a0d901c
mmxzps <mojtaba.mobasheri@hotmail.com>
2025-03-31 12:26:15 +0200

    added icollection and update mgration

--- VerifyIdentityAPI/Migrations/20250331100431_added-ICollection.Designer.cs
+++ VerifyIdentityAPI/Migrations/20250331100431_added-ICollection.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VerifyIdentityAPI.Data;
 
@@ -11,9 +12,11 @@ using VerifyIdentityAPI.Data;
 namespace VerifyIdentityAPI.Migrations
 {
     [DbContext(typeof(VerifyIdentityDbContext))]
-    partial class VerifyIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250331100431_added-ICollection")]
+    partial class addedICollection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
