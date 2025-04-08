0098a052efbc6c905ae481deca2ef5a797be9fce
mmxzps <mojtaba.mobasheri@hotmail.com>
2025-04-08 19:31:42 +0200

    added asp.net web app with individual account identity

---
+++ VerifyIdentityAspFrontend/Models/AppRegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using VerifyIdentityAspFrontend.Areas.Identity.Pages.Account;
+
+namespace VerifyIdentityAspFrontend.Models
+{
+    public class AppRegisterModel : RegisterModel
+    {
+        public AppRegisterModel(UserManager<IdentityUser> userManager, IUserStore<IdentityUser> userStore, SignInManager<IdentityUser> signInManager, ILogger<RegisterModel> logger, IEmailSender emailSender) 
+            : base(userManager, userStore, signInManager, logger, emailSender)
+        {
+        }
+
+        public string Tibia { get; set; }
+    }
+}
