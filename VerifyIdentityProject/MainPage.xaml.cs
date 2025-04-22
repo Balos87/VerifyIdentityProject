cf2b51e1f05a351d51bccd960c8ed58d070f6a1d
mmxzps <mojtaba.mobasheri@hotmail.com>
2025-04-22 13:48:31 +0200

    deleted unnecessary code

--- VerifyIdentityProject/MainPage.xaml.cs
+++ VerifyIdentityProject/MainPage.xaml.cs
@@ -8,8 +8,6 @@ namespace VerifyIdentityProject
     public partial class MainPage : ContentPage
     {
         private MainPageViewModel _viewModel;
-        private int count;
-        private string? qrData = "";
 
         public MainPage(MainPageViewModel viewModel, INfcReaderManager nfcReaderManager)
         {
@@ -20,53 +18,8 @@ namespace VerifyIdentityProject
 
             _viewModel = viewModel;
             BindingContext = _viewModel;
-
-            //for QR-code reader
-            //barcodeReader.Options = new ZXing.Net.Maui.BarcodeReaderOptions
-            //{
-            //    Formats = ZXing.Net.Maui.BarcodeFormat.QrCode,
-            //    AutoRotate = true,
-            //    Multiple = true,
-            //    TryHarder = true,
-            //};
         }
 
-        //private void barcodeReader_BarcodesDetected(object sender, ZXing.Net.Maui.BarcodeDetectionEventArgs e)
-        //{
-        //    //runs it on the main thread. 
-        //    MainThread.BeginInvokeOnMainThread(() =>
-        //    {
-        //        qrData = e.Results.FirstOrDefault()?.Value;
-        //        barcodeReader.IsVisible = false;
-        //        barcodeReader.IsDetecting = false;
-        //        cancelButton.IsEnabled = false;
-        //        DisplayAlert("QR data:", qrData, "OK");
-        //    });
-        //}
-
-        //private async void OnStartScanningClicked(object sender, EventArgs e)
-        //{
-        //    Permissions.RequestAsync<Permissions.Camera>();
-        //    var status =  Permissions.CheckStatusAsync<Permissions.Camera>();
-        //    if (status.Result.Equals("Granted") )
-        //    {
-        //        Console.WriteLine("Asking for Camera permission...");
-        //        status = Permissions.RequestAsync<Permissions.Camera>();
-        //        Console.WriteLine($"New Camera permission status: {status}");
-        //    }
-        //    await Task.Delay(100);
-        //    barcodeReader.IsDetecting = true;
-        //    barcodeReader.IsVisible = true;
-        //    cancelButton.IsEnabled = true;
-        //}
-
-        //private void CancelScanning(object sender, EventArgs e)
-        //{
-
-        //    barcodeReader.IsVisible = false;
-        //    barcodeReader.IsDetecting = false;
-        //    cancelButton.IsEnabled = false;
-        //}
 
         private void GotoScanQrPage(object sender, EventArgs e)
         {
